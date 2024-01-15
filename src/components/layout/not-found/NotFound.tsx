25c70e33e96f6ab81bff5eaefbc7b5d89fb986eb
WildPastry <mparkernzdev@gmail.com>
2024-01-15 20:31:02 +1300

    Add temp overrides to inputs

--- src/components/layout/not-found/NotFound.tsx
+++ src/components/layout/not-found/NotFound.tsx
@@ -1,20 +1,16 @@
 import { Button, Container, Group, Text, Title } from '@mantine/core';
 import { Illustration } from './Illustration';
 import { Link } from 'react-router-dom';
-import classes from './NotFound.module.css';
+import styles from './NotFound.module.css';
 
 const NotFound: React.FC = (): JSX.Element => {
   return (
     <Container className='w-full p-4'>
-      <div className={classes.inner}>
-        <Illustration className={classes.image} />
-        <div className={classes.content}>
-          <Title className={classes.title}>Nothing to see here</Title>
-          <Text
-            c='dimmed'
-            size='lg'
-            ta='center'
-            className={classes.description}>
+      <div className={styles.inner}>
+        <Illustration className={styles.image} />
+        <div className={styles.content}>
+          <Title className={styles.title}>Nothing to see here</Title>
+          <Text c='dimmed' size='lg' ta='center' className={styles.description}>
             The page you are trying to open does not exist. You may have
             mistyped the address, or the page has been moved to another URL. If
             you think this is an error contact support.
