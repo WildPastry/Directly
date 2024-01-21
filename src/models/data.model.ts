d2c16f2069c0d518877c3fd7275e78677bf33f93
WildPastry <mparkernzdev@gmail.com>
2024-01-22 10:14:24 +1300

    Add sortable list for uploaded file types

--- src/models/data.model.ts
+++ src/models/data.model.ts
@@ -1,4 +1,18 @@
+import { FileWithPath } from "@mantine/dropzone";
+
 export interface ILoading {
   isLoading: boolean;
   isError: boolean;
+}
+
+export interface ISortableItem {
+  id: number;
+  name: string;
+  size: number;
+  type: string;
+  lastModified: number;
+}
+
+export interface ISortableItems {
+  files: FileWithPath[];
 }
--- src/pages/books/Books.tsx
+++ src/pages/books/Books.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import { DropzoneArea } from '../../components/features/dropzone/Dropzone';
 import { FileWithPath } from '@mantine/dropzone';
-import { useState } from 'react';
 import SortableList from '../../components/layout/list/List';
+import { useState } from 'react';
 
 const Books: React.FC = (): JSX.Element => {
   const [files, setFiles] = useState([] as FileWithPath[]);
@@ -17,23 +17,7 @@ const Books: React.FC = (): JSX.Element => {
   return (
     <section aria-label='Books Section'>
       <DropzoneArea fileUploaded={handleFiles} className='mb-3' />
-      <SortableList />
-      {display ? (
-        <div className='flex'>
-          {files.map((currentFile: FileWithPath) => (
-            <div key={currentFile.name}>
-              <p>{currentFile.name ? currentFile.name : 'Not available'}</p>
-              <p>{currentFile.size ? currentFile.size : 'Not available'}</p>
-              <p>{currentFile.type ? currentFile.type : 'Not available'}</p>
-              <p>
-                {currentFile.lastModified
-                  ? currentFile.lastModified
-                  : 'Not available'}
-              </p>
-            </div>
-          ))}
-        </div>
-      ) : null}
+      {display ? <SortableList files={files} /> : null}
     </section>
   );
 };
